54537d26ca53914d6658b01adc25fb913c7cabd6
29payal <111033408+29payal@users.noreply.github.com>
2022-09-01 08:51:54 -0400

    Update PerfectNumber.cs

--- LogicalPrograms/PerfectNumber.cs
+++ LogicalPrograms/PerfectNumber.cs
@@ -15,7 +15,7 @@ namespace LogicalPrograms
             Console.WriteLine("Enter the number you want to check perfect number or not");
             int N;
             N = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(N);
+            Console.WriteLine("The positive divisor : " +N);
             int sum = 0;
 
 
@@ -28,7 +28,7 @@ namespace LogicalPrograms
                 }
 
             }
-            Console.WriteLine(N);
+            Console.WriteLine("The sum of the divisor is : " +sum);
             if (N == sum)    // here checking the number is equal to sum of the divisible number 
             {
                 Console.WriteLine("the {0} is Perfect NUmber ", N);  // if yes the number is perfect number
