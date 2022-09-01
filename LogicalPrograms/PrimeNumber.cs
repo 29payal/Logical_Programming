775c933101f70d77761261935178989e3fd54dd6
Payal Sharma <https://github.com/29payal>
2022-09-01 09:04:26 -0400

    [Payal] UC3-PrimeNumber

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public static void primeNumber()
+        {
+
+            Console.WriteLine("Enter the number to check prime number or not");
+            int num = Convert.ToInt32(Console.ReadLine()); //taking inout from user
+            int flag = 0;
+
+            for (int i = 2; i <= num / 2; i++)     // checking the number is prime or not by looping the half number 
+            {
+                if (num % i == 0)  //here if number is divided by ith number then it is not prime number 
+                {
+                    Console.WriteLine(" {0} is not a prime number", num);
+                    flag = 1;    //here declear the flag =1;
+                }
+
+            }
+            if (flag == 0)   //flag ==0 then  it is prime number
+            {
+                Console.WriteLine(" {0} is a prime number", num);
+
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,15 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose the option\n 1. Fibonnacci Series\t 2.Perfect NUmber ");
+            Console.WriteLine("Choose the option\n 1. Fibonnacci Series\t 2.Perfect NUmber\t 3.Prime Number ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     FibonnacciSeries.Fibonnacci();
                     break;
-                    case 2:
+                case 2:
                     PerfectNumber.perfectnumber();
+                    break;
+                case 3:
+                    PrimeNumber.primeNumber();
                     break;
                     default:
                     Console.WriteLine("Wrong Choice");
