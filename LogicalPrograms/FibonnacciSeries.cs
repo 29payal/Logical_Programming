8a90997e6f097e75de3eae7e1c4dc3f7ffb622fe
Payal Sharma <https://github.com/29payal>
2022-09-01 07:58:44 -0400

    [Payal] UC1-FibonnacciSeries

---
+++ LogicalPrograms/FibonnacciSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonnacciSeries
+    {
+        public static void Fibonnacci()
+        {
+            Console.WriteLine("Enter the number you want to Fibonnacci series");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int result = 0;
+            Console.WriteLine("The fibonnacci series: ");
+            Console.WriteLine(a);
+            Console.WriteLine(b);   
+      
+            for (int i = 0; i < number; i++)
+            {
+                result = a + b;
+                Console.WriteLine("{0}" , result);  
+                a = b;
+                b = result; 
+
+            }
+            
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO LOGICAL PROGRMAS");
+            Console.WriteLine("Choose the option\n 1. Fibonnacci Series\t 2. ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    FibonnacciSeries.Fibonnacci();
+                    break;
+                    case 2:
+                   
+                    break;
+                    default:
+                    Console.WriteLine("Wrong Choice");
+                    break;
+            }
         }
     }
 }
