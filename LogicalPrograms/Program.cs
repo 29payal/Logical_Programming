13732e7b3e8db0e20d7cfe0fcc79c15ab92980d4
Payal Sharma <https://github.com/29payal>
2022-09-11 13:18:20 +0530

    [Payal] UC13-StopWatch

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose the option\n 1. Fibonnacci Series\t 2.Perfect NUmber\t 3.Prime Number\t 4.Reverse nUmber\t 5.Binary Conversion\t 6.Swap nibbles\t 7. Square Root\t 8.Day of Week\t 9.Monthly Payment\t  10. NOTES\t  11. Temperature Conversion\t 12. Coupon number ");
+            Console.WriteLine("Choose the option\n 1. Fibonnacci Series\t 2.Perfect NUmber\t 3.Prime Number\t 4.Reverse nUmber\t 5.Binary Conversion\t 6.Swap nibbles\t 7. Square Root\t 8.Day of Week\t 9.Monthly Payment\t  10. NOTES\t  11. Temperature Conversion\t 12. Coupon number\t 13. Stop__Watch ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -43,6 +43,9 @@
                     break;
                 case 12 :
                     CouponNumber.GetCouponNumber();
+                    break;
+                case 13 :
+                    StopWatch.stopwatch();
                     break;
                     default:
                     Console.WriteLine("Wrong Choice");
